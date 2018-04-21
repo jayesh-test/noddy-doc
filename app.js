395c6b74fa5a01e1f59bd4f09828f1134ec53eb0
jayesh sapkale <node.jayesh@gmail.com>
2018-04-22 02:03:02 +0530

    Test

--- app.js
+++ app.js
@@ -127,39 +127,148 @@ function map (a,f) {
 
 
 
+function scrape_from_youtube(video_id,callback) {
+
+var http = require('https');
+var fs = require('fs');
+
+  https.get("https://pickvideo.net/download?video="+video_id,function(res) {
+    var chunks = [];
+    res.on('data', function(chunk){
+      chunks.push(chunk);
+    }).on('end', function(){
 
 
-app.get("/ytb/scrape1",function(req,res1){
-
-  var http = require('https')
-  var fs = require('fs')
-
-  https.get("https://pickvideo.net/download?video=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DQ1TXk7ZdiQE",function(res) {
-        var chunks = []
-        res.on('data', function(chunk){chunks.push(chunk)
-        }).on('end', function(){
-
+              var data = Buffer.concat(chunks).toString();
+              
+              var cheerio = require('cheerio');
+              var $ = cheerio.load(data);
+              var format = {};
+              var dom = $(".downloadsTable").first().find("tr td:first-child");
+              //var expire_link_timstamp_regex=/(?<=expire=)(.*)\d{10}/gmi;
+              //var expire_link_timstamp_regex=/expire=\d{10}/gmi;
 
 
-          var data = Buffer.concat(chunks).toString()
-          fs.writeFile(process.cwd()+"/pick_video.html",data,function(){
-      //res1.send("Ok");
-      //return 0;
-    }); 
+                  dom.each(function(i,val){
 
-          res1.send(data);
+                     //var expire_timestamp_val = $(val).next().next().next().find("a").attr("href");
 
-          //console.log(data);
+                      //console.log(expire_timestamp[0]);
+
+                      format[$(val).html()]={link:$(val).next().next().next().find("a").attr("href"),size:$(val).next().next().html(),format:$(val).next().html()} 
+
+
+                     // if(expire_timestamp[0]){
+                     //  expire_timestamp=expire_timestamp[0].split("=")[1];
+                     //  format[$(val).html()]={expire:expire_timestamp[0],link:$(val).next().next().next().find("a").attr("href"),size:$(val).next().next().html(),format:$(val).next().html()}
+                     // }else{
+                     //  format[$(val).html()]={expire:0,link:$(val).next().next().next().find("a").attr("href"),size:$(val).next().next().html(),format:$(val).next().html()} 
+                     // }
+                     
+                  });
+
+                    // var expire_timestamp = expire_link_timstamp_regex.exec(format[Object.keys(format)[0]].link);
+                    // console.log(expire_timestamp);
+                    // console.log(format[Object.keys(format)[0]].link);
+
+
+                  //console.log(data);
+                  callback({format:format});
+                  /*Send*/
+
+              /*Search for dom*/
+              //res1.send(data);
+              //res1.json({response:format});
+              /*Searh dom for 720p*/
+          });
+       });
+
+
+};
+
+
+
+
+
+// app.get("/redis/test",function(req,res){
+
+
+//       var redis = require('redis');
+//   var config = {
+//     port: 19630,
+//     secret: 'secret',
+//     redisConf: {
+//       host: 'redis-19630.c16.us-east-1-2.ec2.cloud.redislabs.com', // The redis's server ip 
+//       port: 19630,
+//       pass: 'MK1xuD8LKfw16UwFmhed73WXd7JDn0mT'
+//     }
+//   };
+// var redisClient = redis.createClient(config.redisConf);
+
+
+//       redisClient.auth('MK1xuD8LKfw16UwFmhed73WXd7JDn0mT',function(err){
+//           console.log(err);
+//       });
+
+
+//        redisClient.on('connect', function() {
+
+//       redisClient.set("test","1",function(err,data){
+//           console.log(err);
+//           redisClient.get("test",function(err,data){
+//             console.log(err);
+//             console.log(data);
+//             res.send(data);
+//           });
+//       });
+
+//       console.log('connected to redis!!');
+//      });
+
+//      redisClient.on('error', function(err) {
+//        console.log("Error...");
+//        console.log(err);
+//      });
+
+
+
+
+// });
+
+
+
+// app.get("/ytb/scrape1",function(req,res1){
+
+//   var http = require('https')
+//   var fs = require('fs')
+
+
+//   https.get("https://pickvideo.net/download?video=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DQ1TXk7ZdiQE",function(res) {
+//         var chunks = []
+//         res.on('data', function(chunk){chunks.push(chunk)
+//         }).on('end', function(){
+
+
+
+//           var data = Buffer.concat(chunks).toString()
+//           fs.writeFile(process.cwd()+"/pick_video.html",data,function(){
+//       //res1.send("Ok");
+//       //return 0;
+//     }); 
+
+//           res1.send(data);
+
+//           //console.log(data);
 
           
-        })
-      }).on('error', function(e) {
-        console.log("Got error: " + e.message)
-      });
+//         })
+//       }).on('error', function(e) {
+//         console.log("Got error: " + e.message)
+//       });
 
 
   
-});
+// });
 
 app.get("/ytb/test_mongo",function(req,res1){
 
@@ -236,87 +345,181 @@ app.get("/ytb/test_mongo",function(req,res1){
 
 });
 
+
+/*Mongo*/
+var MongoClient = require('mongodb').MongoClient;
+var mongo_database = "";
+MongoClient.connect('mongodb://ytb_user_mlab149:ytb_mlab_pwd12@ds247439.mlab.com:47439/ytb_test', function(err,database) {
+  if(err){
+      mongo_database="";
+      console.log(err);
+  }else{
+    console.log("Remote Mongodb connect...");
+      mongo_database = database.db('ytb_test');
+  }
+});
+
+  // var db = null;
+  // MongoClient.connect('mongodb://ytb_user_mlab149:ytb_mlab_pwd12@ds247439.mlab.com:47439/ytb_test', function(err,database) {
+  //          if(err){
+  //             res1.send(err);
+  //          }else{
+  //           console.log(err);
+  //           //console.log(database);
+  //           var db1 = database.db('ytb_test');
+
+  //            db1.collection('ytb').insert({url:[1,2]},function(err,doc) {
+  //               if(err){
+  //              res1.send(err);
+  //           }else{
+
+  //                 db1.collection('ytb').find({}).toArray(function(err,doc) {
+  //                 if(err){
+  //                    res1.send(err);
+  //                 }else{
+  //                   res1.send(JSON.stringify(doc));
+  //                 }
+  //                });
+
+                    
+  //           }
+  //          });
+
+
+
+
+  //         }
+  // });
+
+
+
+// db.ytb.update(
+//    {"ytb_code":1},
+//    {"ytb_code":1,test:"1243"},
+//    {
+//      upsert: true     
+//    }
+// )
+
 app.get("/ytb/scrape",function(req,res1){
 
   var https = require('https');
   var fs = require('fs')
-  //var video_id = "tCddpGLE0aw";
-  //var video_id = "tCddpGLE0aw";
   var video_id = req.query.id;
-  if(video_id){
-       https.get("https://pickvideo.net/download?video="+video_id,function(res) {
-          var chunks = [];
-          res.on('data', function(chunk){
-              chunks.push(chunk);
-          }).on('end', function(){
+  var expire_time = req.query.expire_time;
 
-              var data = Buffer.concat(chunks).toString();
-              //console.log(data);
-              var cheerio = require('cheerio');
-              var $ = cheerio.load(data);
-              var format = {};
-              var dom = $(".downloadsTable").first().find("tr td:first-child");
-                  dom.each(function(i,val){
-                     format[$(val).html()]={link:$(val).next().next().next().find("a").attr("href"),size:$(val).next().next().html(),format:$(val).next().html()}
+  if(video_id){
+      
+      /*No:Scrape fresh and push into mongodb*/
+      mongo_database.collection('ytb').find({ytb_code:video_id,expire:{$gte:expire_time}}).toArray(function(err,doc){
+         if(err){
+            scrape_from_youtube(video_id,function(format_obj){
+                console.log(format_obj);
+                res1.send("Scrape and push");
+            });
+         }else{
+              //console.log(doc);
+
+              if(doc.length>0){
+                  /*Someone already push new epire date just use it*/
+                    console.log("use first document");
+                    //res1.send(JSON.stringify(doc));
+                    var format={};
+                    for(i in doc[0].url){
+                      format[i]={link:doc[0].url[i]};
+                    }
+                    res1.send({status:1,response:{format:format}});
+              }else{
+                  console.log("First user to pull expire url");
+                  scrape_from_youtube(video_id,function(format_obj){
+                    //console.log(format_obj);
+                    //var expire_time = format_obj.format[Object.keys(format_obj.format)[0]].link;
+
+                    var expire_link_timstamp_regex=/expire=\d{10}/gmi;
+                    var expire_timestamp = expire_link_timstamp_regex.exec(format_obj.format[Object.keys(format_obj.format)[0]].link);
+                    //console.log(expire_timestamp[0].toString());
+                    //console.log(expire_timestamp[0].toString().split("=")[1]);
+                    var expire_time=expire_timestamp[0].toString().split("=")[1];
+
+                    var links_url = {};
+                    //console.log("expire_time = "+expire_time);
+
+                    for(i in format_obj.format){
+                        links_url[i]=format_obj.format[i].link;
+                    }
+
+                    /*Push into mongodb*/
+                    mongo_database.collection('ytb').update({"ytb_code":video_id},{"ytb_code":video_id,url:links_url,expire:parseInt(expire_time,10)},{upsert: true },function(err,doc){
+                       if(err){
+                         console.log("Fail to push into mongodb");
+                         console.log(err);
+                       }else{
+                         console.log("okay pushed");
+                       }
+                    });
+                    res1.send({status:1,links_url:links_url,response:format_obj});
                   });
 
-              /*Search for dom*/
-              //res1.send(data);
-              res1.json({response:format});
-              /*Searh dom for 720p*/
-          });
-       });
 
+                  
+                  //res1.send("Scrape and push");
+                  /*Use*/
+                  // scrape_from_youtube(ytb_code,function(format_obj){
+                  // });
+              }
+         }
+      });
+   }else{
+     res1.send({status:0,err:"Video id not passed"});
    }
-
 });
 
 
-app.get("/ytb/scrape",function(req,res1){
+// app.get("/ytb/scrape",function(req,res1){
 
-  var http = require('http')
-  var fs = require('fs')
-  //var video_id = "tCddpGLE0aw";
-  //var video_id = "tCddpGLE0aw";
-  var video_id = req.query.id;
+//   var http = require('http')
+//   var fs = require('fs')
+//   //var video_id = "tCddpGLE0aw";
+//   //var video_id = "tCddpGLE0aw";
+//   var video_id = req.query.id;
 
-  if(video_id){
+//   if(video_id){
 
 
-       http.get("http://www.youtube.com/get_video_info?video_id="+video_id, function(res) {
-          var chunks = [];
-        res.on('data', function(chunk){chunks.push(chunk)
+//        http.get("http://www.youtube.com/get_video_info?video_id="+video_id, function(res) {
+//           var chunks = [];
+//         res.on('data', function(chunk){chunks.push(chunk)
 
-        }).on('end', function(){
-          var data = Buffer.concat(chunks).toString()
-           var videoInfo = parseVideoInfo(data)
-          if(videoInfo.title=="-"){
-            console.log(data);
-            //res1.send("not ok");
-            res1.send({video_id:video_id,videoInfo:[]});
-          }else{
-            //downloadVideo({videoInfo:videoInfo});
+//         }).on('end', function(){
+//           var data = Buffer.concat(chunks).toString()
+//            var videoInfo = parseVideoInfo(data)
+//           if(videoInfo.title=="-"){
+//             console.log(data);
+//             //res1.send("not ok");
+//             res1.send({video_id:video_id,videoInfo:[]});
+//           }else{
+//             //downloadVideo({videoInfo:videoInfo});
 
-            /*Send this urls to user*/
-            //res1.send({video_id:video_id,videoInfo:videoInfo});
-            var videoInfo = parseVideoInfo(data)
+//             /*Send this urls to user*/
+//             //res1.send({video_id:video_id,videoInfo:videoInfo});
+//             var videoInfo = parseVideoInfo(data)
 
-            downloadVideo({res1:res1,videoInfo:videoInfo});
-            //res1.send("ok");
-          }
+//             downloadVideo({res1:res1,videoInfo:videoInfo});
+//             //res1.send("ok");
+//           }
           
-        })
-      }).on('error', function(e) {
-        console.log("Got error: " + e.message)
-      });
+//         })
+//       }).on('error', function(e) {
+//         console.log("Got error: " + e.message)
+//       });
 
-  }
+//   }
 
 
 
   
 
-});
+// });
 
 
 
@@ -366,7 +569,7 @@ app.get("/ytb/lot",function(req,res){
                             item =  item.substring(item.indexOf("-")+1,item.indexOf("."));
                             file_list.push(item);
                         }
-                      }); 
+                      });
                       async_recall(null,{version_history:file_list,version:version,lot:JSON.parse(data)});
                     }
                 });
