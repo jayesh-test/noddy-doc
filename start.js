d71593d752dc766f1b0486cb9bf0246cc604d2dc
jayesh sapkale <node.jayesh@gmail.com>
2018-04-26 22:38:04 +0530

    Test

--- start.js
+++ start.js
@@ -6,20 +6,22 @@ var cluster = require('cluster');
 var http = require('http');
 var numCPUs = require('os').cpus().length;
 
+ app.listen(port); 
+
 //console.log("CPU count = "+numCPUs);
 
-if (cluster.isMaster) {
-  // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// if (cluster.isMaster) {
+//   // Fork workers.
+//   for (var i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-  });
-} else {  
-  app.listen(port);  
-}
+//   cluster.on('exit', function(worker, code, signal) {
+//     console.log('worker ' + worker.process.pid + ' died');
+//   });
+// } else {  
+//   app.listen(port);  
+// }
 
 
 /*single process*/
