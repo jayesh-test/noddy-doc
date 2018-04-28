541da8d8ff64d6a9d55e4ccdba9493cab5f0b1d7
jayesh sapkale <node.jayesh@gmail.com>
2018-04-28 06:54:26 +0530

    Test

--- app.js
+++ app.js
@@ -1110,7 +1110,7 @@ app.get("/ytb/scrape",function(req,res1){
                      //    auto_mongo();
                      // }
 
-                    res1.send({status:1,links_url:links_url,response:format_obj,expire_time:expire_time});
+                    res1.send({status:1,links_url:links_url,response:format_obj,expire_time:expire_time,fire_lighter:1});
                   });
 
               }
